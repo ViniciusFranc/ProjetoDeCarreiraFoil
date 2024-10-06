f91cd2942089dd9065e3530e14bbf539f29d3eda
Vinícius Secretti Franceschi <vinicius.franceschi4@gmail.com>
2024-10-06 10:49:37 -0300

    Teste

--- src/index.js
+++ src/index.js
@@ -5,22 +5,24 @@ const port = 3000;
 var path = require('path');
 const app = express();
 
-<<<<<<< HEAD
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
-=======
-app.get("/",(req,res) => {
-    res.send('...')
-})
->>>>>>> 7006065f768febfd822a0a518ec9b434ea87f083
 
 
 server.listen(port, () => {
     console.log('exemple test')
-<<<<<<< HEAD
 });
-=======
+
+const express = require ("express")
+const app = express()
+const port = 3000
+
+app.get("/",(req,res) => {
+    res.send('...')
 })
->>>>>>> 7006065f768febfd822a0a518ec9b434ea87f083
+
+app.listen(port, () => {
+    console.log('exemple test')
+})
