31e2a894979ba7feb0ad0901b9dc4ff53edd16a1
Vinícius Secretti Franceschi <vinicius.franceschi4@gmail.com>
2024-10-06 12:23:45 -0300

    Api login Host local

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 const express = require ("express");
-const session = require ("express-session");
- 
+const session = require ('express-session');
+ const bodyparser = require('body-parser');
+
+
+
 const port = 3000;
 var path = require('path');
 const app = express();
+
+
+var login = "admin";
+var password = "123456"
+
+
+app.use(session({secret:'yfbeir34131iuw'}));
+app.use(bodyparser.urlencoded({extended:true}));
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -11,18 +22,31 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/views'));
 
 
-server.listen(port, () => {
-    console.log('exemple test')
-});
+app.post('/', (req,res)=>{
+    if (req.body.password == password && req.body.login == login){
+        // user logado
+        req.session.login = login;
+        
+        res.render('logado')
 
-const express = require ("express")
-const app = express()
-const port = 3000
-
-app.get("/",(req,res) => {
-    res.send('...')
+    }else{
+        res.render('index');
+    }
 })
+
+
+app.get('/' , (req,res) => {
+    if (req.session.login){
+        res.render('logado')
+    }else{
+        req.render('index')
+    }
+
+    res.render('index');
+})
+
 
 app.listen(port, () => {
-    console.log('exemple test')
+    console.log('exemple test');
 })
+
